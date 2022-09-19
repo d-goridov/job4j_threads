c16ee84dc8023cba90cecbd798b51f3706d6d522
d.goridov <dimagoridov@yandex.ru>
2022-09-19 22:21:44 +0300

    4. ThreadSafe динамический список [#1105] fix #1

--- src/test/java/ru/job4j/synch/SingleLockListTest.java
+++ src/test/java/ru/job4j/synch/SingleLockListTest.java
@@ -3,6 +3,7 @@ package ru.job4j.synch;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -23,5 +24,17 @@ public class SingleLockListTest {
         Set<Integer> rsl = new TreeSet<>();
         list.iterator().forEachRemaining(rsl::add);
         assertThat(rsl, is(Set.of(1, 2)));
+    }
+
+    @Test
+    public void workIterator() {
+        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Iterator<Integer> it = list.iterator();
+        it.next();
+        list.add(5);
+        it.next();
     }
 }
