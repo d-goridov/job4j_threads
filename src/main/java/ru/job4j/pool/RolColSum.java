8a7cb478352fd6747e67a0964405a184ec793523
d.goridov <dimagoridov@yandex.ru>
2022-10-04 11:49:20 +0300

    4. CompletableFuture [#361626] fix #1

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -1,48 +1,11 @@
 package ru.job4j.pool;
 
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
 
-    public static class Sums {
-
-        private int rowSum;
-        private int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Sums sums = (Sums) o;
-            return rowSum == sums.rowSum && colSum == sums.colSum;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(rowSum, colSum);
-        }
-    }
-
-    private static Sums getSumsForIndex(int[][] matrix, int index) {
+    private Sums getSumsForIndex(int[][] matrix, int index) {
         int sumRow = 0;
         int sumCol = 0;
         for (int j = 0; j < matrix.length; j++) {
@@ -52,7 +15,7 @@ public class RolColSum {
         return new Sums(sumRow, sumCol);
     }
 
-    public static Sums[] sum(int[][] matrix) {
+    public Sums[] sum(int[][] matrix) {
         Sums[] rsl = new Sums[matrix.length];
         for (int i = 0; i < rsl.length; i++) {
             rsl[i] = getSumsForIndex(matrix, i);
@@ -60,7 +23,7 @@ public class RolColSum {
         return rsl;
     }
 
-    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+    public  Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
         Sums[] rsl = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             int index = i;
--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -11,21 +11,21 @@ class RolColSumTest {
     @Test
     public void whenSyncCalculateSum() {
         int[][] matrix = {{1, 2}, {3, 4}};
-
-        assertThat(RolColSum.sum(matrix))
-                .isEqualTo(new RolColSum.Sums[]{
-                        new RolColSum.Sums(3, 4),
-                        new RolColSum.Sums(7, 6)});
+        RolColSum rsl = new RolColSum();
+        assertThat(rsl.sum(matrix))
+                .isEqualTo(new Sums[]{
+                        new Sums(3, 4),
+                        new Sums(7, 6)});
     }
 
     @Test
     public void whenAsyncCalculateSum() throws ExecutionException, InterruptedException {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-        assertThat(RolColSum.asyncSum(matrix))
-                .isEqualTo(new RolColSum.Sums[]{
-                        new RolColSum.Sums(6, 12),
-                        new RolColSum.Sums(15, 15),
-                        new RolColSum.Sums(24, 18)});
+        RolColSum rsl = new RolColSum();
+        assertThat(rsl.asyncSum(matrix))
+                .isEqualTo(new Sums[]{
+                        new Sums(6, 12),
+                        new Sums(15, 15),
+                        new Sums(24, 18)});
     }
 }
