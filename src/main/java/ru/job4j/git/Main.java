2f70c4a570065998749b5ef702a11a5b912b0d1c
d.goridov <dimagoridov@yandex.ru>
2022-10-09 15:24:35 +0300

    3.2. Откат изменений [#504830] add names

--- src/main/java/ru/job4j/git/Main.java
+++ src/main/java/ru/job4j/git/Main.java
@@ -3,5 +3,8 @@ package ru.job4j.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Dmitriy");
+        System.out.println("Petr");
+        System.out.println("Stanislav");
     }
 }
